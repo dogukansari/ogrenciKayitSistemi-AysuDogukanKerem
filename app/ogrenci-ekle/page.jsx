21aca3da69c91785d62233205b89802182607f50
Kerem Kacmaz <keremkacmaz600@gmail.com>
2024-09-29 14:10:12 +0300

    Öğrenci adı, soyadı, telefon, e-posta adresi ve doğum tarihi alanları eklendi

--- app/ogrenci-ekle/page.jsx
+++ app/ogrenci-ekle/page.jsx
@@ -1,9 +1,26 @@
 "use client"
 
 export default function OgrenciEkle() {
-  return(
+  return (
     <>
       <h1>Sertifika Uygulaması</h1>
+      <form action="">
+     <label htmlFor="">Öğrenci Adı</label>
+        <input type="text" id="ogrenciAdi" name="ogrenciAdi" required />
+        <br />
+
+     <label htmlFor="" >Öğrenci Soyadı</label>
+        <input type="text" id="ogrenciSoyadi" name="ogrenciSoyadi" required />
+        <br />
+     <label htmlFor="" >Öğrenci Telefonu</label>
+        <input type="text" id="ogrenciTelefon" name="ogrenciTelefon" required />
+          <br />
+     <label htmlFor="" >Öğrenci E-Posta Adresi</label>
+        <input type="email" id="ogrenciMail" name="ogrenciMail" required />
+       <br />       
+     <label htmlFor="doğumTarihi">Doğum Tarihi</label>
+        <input type="date" id="doğumTarihi" name="doğumTarihi" required />
+      </form>
     </>
   )
 }
