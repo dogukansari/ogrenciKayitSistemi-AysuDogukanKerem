de05bb40bce1e2dd72c73f89f6f051161bfad5ce
Doğukan Sarı <dogukansari@Dogukan-MacBook-Pro.local>
2024-09-29 14:29:43 +0300

    Satırdaki boşluklar düzeltildi.

--- app/ogrenci-ekle/page.jsx
+++ app/ogrenci-ekle/page.jsx
@@ -5,20 +5,20 @@ export default function OgrenciEkle() {
     <>
       <h1>Sertifika Uygulaması</h1>
       <form action="">
-     <label htmlFor="">Öğrenci Adı</label>
+        <label htmlFor="">Öğrenci Adı</label>
         <input type="text" id="ogrenciAdi" name="ogrenciAdi" required />
         <br />
 
-     <label htmlFor="" >Öğrenci Soyadı</label>
+        <label htmlFor="" >Öğrenci Soyadı</label>
         <input type="text" id="ogrenciSoyadi" name="ogrenciSoyadi" required />
         <br />
-     <label htmlFor="" >Öğrenci Telefonu</label>
+        <label htmlFor="" >Öğrenci Telefonu</label>
         <input type="text" id="ogrenciTelefon" name="ogrenciTelefon" required />
           <br />
-     <label htmlFor="" >Öğrenci E-Posta Adresi</label>
+        <label htmlFor="" >Öğrenci E-Posta Adresi</label>
         <input type="email" id="ogrenciMail" name="ogrenciMail" required />
        <br />       
-     <label htmlFor="doğumTarihi">Doğum Tarihi</label>
+        <label htmlFor="doğumTarihi">Doğum Tarihi</label>
         <input type="date" id="doğumTarihi" name="doğumTarihi" required />
       </form>
     </>
